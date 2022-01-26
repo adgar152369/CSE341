ad973e50e8dc776f23487f207603768205673030
Adam Garcia <adgar1523@gmail.com>
2022-01-26 00:41:54 -0700

    working with mongoDB

--- week04/util/database.js
+++ week04/util/database.js
@@ -1,8 +1,28 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+// create connection to mongodb
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb+srv://adgar1523:092696cod@cluster0.zygoz.mongodb.net/shop?retryWrites=true&w=majority')
+  .then(client => {
+    console.log('connected');
+    _db = client.db();
+    callback();
+  })
+  .catch((err) => {
+    console.log(err);
+    throw err;
+  })
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
