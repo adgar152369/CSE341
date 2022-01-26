0cdf59503aa3938ffd833c84f002fd61a2e6aacb
Adam Garcia <adgar1523@gmail.com>
2022-01-25 20:40:41 -0700

    update week04

--- week04/controllers/admin.js
+++ week04/controllers/admin.js
@@ -13,14 +13,16 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description
-  })
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description
+    })
     .then(result => {
-      console.log('created product');
+      // console.log(result);
+      console.log('Created Product');
       res.redirect('/admin/products');
     })
     .catch(err => {
@@ -34,19 +36,22 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findAll({where: {id: prodId}})
+  req.user
+    .getProducts({ where: { id: prodId } })
+    // Product.findById(prodId)
     .then(products => {
-      if (!products) {
+      const product = products[0];
+      if (!product) {
         return res.redirect('/');
       }
       res.render('admin/edit-product', {
         pageTitle: 'Edit Product',
-        path: 'admin/edit-product',
+        path: '/admin/edit-product',
         editing: editMode,
-        product: products[0]
-      })
+        product: product
+      });
     })
-    .catch();
+    .catch(err => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -55,42 +60,42 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findAll({where: {id: prodId}})
-    .then(products => {
-      products[0].title = updatedTitle;
-      products[0].price = updatedPrice,
-      products[0].description = updatedDesc;
-      products[0].imageUrl = updatedImageUrl;
-      return products[0].save();
+  Product.findById(prodId)
+    .then(product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDesc;
+      product.imageUrl = updatedImageUrl;
+      return product.save();
     })
     .then(result => {
-      console.log('updated product');
+      console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
     })
     .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-  const prodId = req.params.productId;
-  Product.findAll()
+  req.user
+    .getProducts()
     .then(products => {
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
         path: '/admin/products'
-      })
+      });
     })
     .catch(err => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findAll({where: {id: prodId}})
-    .then(products => {
-      return products[0].destroy();
+  Product.findById(prodId)
+    .then(product => {
+      return product.destroy();
     })
     .then(result => {
-      console.log('destroyed');
+      console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
     })
     .catch(err => console.log(err));
--- week04/util/database.js
+++ week04/util/database.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
 
-// setup connection pool to the database
-const sequelize = new Sequelize('node-complete', 'root', 'mysqlserver', {dialect: 'mysql', host: 'localhost'});
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
 
 module.exports = sequelize;
