7e4d1de793f8cd625ee07c03fffa2b905a6f37af
Adam Garcia <adgar1523@gmail.com>
2022-01-31 13:01:18 -0700

    update week04

--- week05/app.js
+++ week05/app.js
@@ -34,7 +34,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+    'mongodb+srv://adgar1523:092696cod@cluster0.zygoz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
   )
   .then(result => {
     User.findOne().then(user => {
