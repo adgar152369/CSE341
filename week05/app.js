0d901e6033e47d27b2d957dc4b3a1eb01aa58e12
Adam Garcia <adgar1523@gmail.com>
2022-02-05 14:09:17 -0700

    update week05

--- week05/app.js
+++ week05/app.js
@@ -3,20 +3,37 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://adgar1523:092696cod@cluster0.zygoz.mongodb.net/shop';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+// session middleware
+app.use(session({
+  secret: 'my secret',
+  resave: false,
+  saveUninitializedL: false,
+  store: store
+}));
 
 app.use((req, res, next) => {
   User.findById('61fa9bdd3b9a09bc06589a70')
@@ -29,12 +46,13 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://adgar1523:092696cod@cluster0.zygoz.mongodb.net/shop?retryWrites=true&w=majority'
+    MONGODB_URI
   )
   .then(result => {
     User.findOne().then(user => {
